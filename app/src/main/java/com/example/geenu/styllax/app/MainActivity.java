12c1dd7fbbca86ad1acc022023c7a55dcecf8f36
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2015-04-01 23:14:30 +0530

    3 Image view

--- app/src/main/java/com/example/geenu/styllax/app/MainActivity.java
+++ app/src/main/java/com/example/geenu/styllax/app/MainActivity.java
@@ -1,82 +1,50 @@
 package com.example.geenu.styllax.app;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+public class MainActivity extends Activity {
 
-
-public class MainActivity extends ActionBarActivity {
+    Button button, button1;
+    ImageView image, image1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+
+        addListenerOnButton();
+
     }
 
+    public void addListenerOnButton() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+        image = (ImageView) findViewById(R.id.imageView1);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        button = (Button) findViewById(R.id.btnNext);
+        button.setOnClickListener(new OnClickListener() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+              @Override
+              public void onClick(View arg0) {
+              image.setImageResource(R.drawable.image2);
+              }
+           });
+
+
+        image1 = (ImageView) findViewById(R.id.imageView1);
+
+        button1 = (Button) findViewById(R.id.btnPrevious);
+        button1.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                image.setImageResource(R.drawable.image5);
+            }
+        });
+
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            Integer[] imageId = {
-                    R.drawable.image1,
-                    R.drawable.image2,
-                    R.drawable.image3,
-                    R.drawable.image4,
-                    R.drawable.image5,
-                    R.drawable.image6,
-                    R.drawable.image7
-            };
-
-            List<Integer> images = new ArrayList<Integer>(
-                    Arrays.asList(imageId));
-
-            return rootView;
-        }
-    }
 }
