36f2ee49fde941c33c68c46ceb0f98404aa08b3e
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2015-04-03 13:54:25 +0530

    set_as_wallpaper added

--- app/src/main/java/com/example/geenu/styllax/app/MainActivity.java
+++ app/src/main/java/com/example/geenu/styllax/app/MainActivity.java
@@ -1,39 +1,57 @@
 package com.example.geenu.styllax.app;
+
 import android.app.Activity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     Button button, button1;
     ImageView image, image1;
 
+    Bitmap bitmap;
+    int lastImageRef;
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addListenerOnButton();
+        Button buttonSetWallpaper = (Button)findViewById(R.id.setWallpaper);
+        ImageView imagePreview = (ImageView)findViewById(R.id.imageView1);
+        imagePreview.setImageResource(R.drawable.image1);
 
-    }
+        buttonSetWallpaper.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
 
-    public void addListenerOnButton() {
+                WallpaperManager myWallpaperManager
+                        = WallpaperManager.getInstance(getApplicationContext());
+                try {
+                    myWallpaperManager.setResource(+R.drawable.image1);
+                } catch (IOException e) {
 
+                    e.printStackTrace();
+                }
+            }});
         image = (ImageView) findViewById(R.id.imageView1);
 
         button = (Button) findViewById(R.id.btnNext);
         button.setOnClickListener(new OnClickListener() {
 
-              @Override
-              public void onClick(View arg0) {
-              image.setImageResource(R.drawable.image2);
-              }
-           });
-
-
+            @Override
+            public void onClick(View arg0) {
+                image.setImageResource(R.drawable.image2);
+            }
+        });
         image1 = (ImageView) findViewById(R.id.imageView1);
 
         button1 = (Button) findViewById(R.id.btnPrevious);
@@ -44,7 +62,6 @@ public class MainActivity extends Activity {
                 image.setImageResource(R.drawable.image5);
             }
         });
-
-        }
+    }
 
 }
